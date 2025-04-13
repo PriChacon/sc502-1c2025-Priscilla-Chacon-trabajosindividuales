db6f96bdacbef1e36b66486b81a609dcc4091ebe
Pri <priscilla.chaconconejo@hotmail.com>
2025-04-13 15:10:49 -0600

    Estudio de Caso2

---
+++ Caso de Estudio 2/app-tareas/backend/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$dbname = "todo_app";
+$user = "root";
+$password = "root1";
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname",username: $user, password: $password);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+   // echo "Conexion exitosa ". PHP_EOL ;
+
+}catch(PDOException $e) {
+    die("Error de conexion: " . $e -> getMessage());
+
+}
